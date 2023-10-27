f850104a1c0721317f18a74d2c2703a6a864afbf
jfisher2021 <j.fisher.2021@alumnos.urjc.es>
2023-10-27 17:35:42 +0200

    cambio graph

--- p2/graph.py
+++ p2/graph.py
@@ -20,39 +20,39 @@ filename = sys.argv[1]
 
 # Define a dictionary to map cases to their corresponding actions
 if filename == 'NRTB':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='blue',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='blue',
                           alpha=0.5, edgecolor='black', label="Non Real Time - bonnie++")
     plt.title('cyclictestURJC RaspberryPi - Kernel No RT', fontsize=40)
 
 elif filename == 'NRTH':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='red',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='red',
                           alpha=0.5, edgecolor='black', label="Non Real Time - hackbench")
     plt.title('cyclictestURJC RaspberryPi - Kernel No RT', fontsize=40)
 
 elif filename == 'NRTI':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='green',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='green',
                           alpha=0.5, edgecolor='black', label="Non Real Time - idle")
     plt.title('cyclictestURJC RaspberryPi - Kernel No RT', fontsize=40)
 
 elif filename == 'RTB':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='blue',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='blue',
                           alpha=0.5, edgecolor='black', label="Real Time - bonnie++")
     plt.title('cyclictestURJC RaspberryPi - Kernel RT', fontsize=40)
 
 
 elif filename == 'RTH':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='red',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='red',
                           alpha=0.5, edgecolor='black', label="Real Time - hackbench")
     plt.title('cyclictestURJC RaspberryPi - Kernel RT', fontsize=40)
 
 elif filename == 'RTI':
-    data['LATENCIA'] = data['LATENCIA'] / 1000
-    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 0.05], color='green',
+    data['LATENCIA'] = data['LATENCIA'] / 1000000
+    data['LATENCIA'].hist(bins=100, ax=ax, range=[0.00, 50.0], color='green',
                           alpha=0.5, edgecolor='black', label="Real Time - idle")
     plt.title('cyclictestURJC RaspberryPi - Kernel RT', fontsize=40)
 
